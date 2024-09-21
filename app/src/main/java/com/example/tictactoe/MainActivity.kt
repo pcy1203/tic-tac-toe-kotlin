dd8a2fdb21f3aa5de89c008bb67a8acea57550d8
pcy1203 <pcy1203@snu.ac.kr>
2024-09-22 01:36:35 +0900

    Tic-Tac-Toe Game Logic & Main Page

---
+++ app/src/main/java/com/example/tictactoe/MainActivity.kt
@@ -0,0 +1,54 @@
+package com.example.tictactoe
+
+import android.os.Bundle
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.Observer
+import com.example.tictactoe.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+    private val viewModel: MainViewModel by viewModels()
+    private lateinit var textViews: List<TextView>
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        textViews = listOf(
+            binding.view1, binding.view2, binding.view3,
+            binding.view4, binding.view5, binding.view6,
+            binding.view7, binding.view8, binding.view9
+        )
+
+        viewModel.board.observe(this, Observer { board ->
+            board.forEachIndexed { index, value ->
+                textViews[index].text = value
+            }
+        })
+
+        viewModel.description.observe(this, Observer { description ->
+            binding.description.text = description
+        })
+
+        textViews.forEachIndexed { index, textView ->
+            textView.setOnClickListener {
+                viewModel.boxClicked(index)
+            }
+        }
+
+        /*
+        enableEdgeToEdge()
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        */
+    }
+}
---
+++ app/src/main/java/com/example/tictactoe/MainViewModel.kt
@@ -0,0 +1,66 @@
+package com.example.tictactoe
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.map
+
+class MainViewModel : ViewModel() {
+
+    private val _board = MutableLiveData<List<String>>(List(9) { "" })
+    val board: LiveData<List<String>> = _board
+
+    private val _description = MutableLiveData<String>("플레이어 1의 차례입니다.")
+    val description: LiveData<String> = _description
+
+    private var end = false
+
+    private var _isFirstPlayer: Boolean = true
+
+    fun boxClicked(index: Int) {
+        if (end) return
+        val currentBoard = _board.value ?: return
+        if (currentBoard[index].isNotEmpty()) return
+        val newBoard = currentBoard.toMutableList()
+        if (_isFirstPlayer) {
+            newBoard[index] = "O"
+            _description.value = "플레이어 2의 차례입니다."
+            if (checkEnd(index, "O")) {
+                _description.value = "게임이 종료되었습니다."
+                end = true
+            }
+        } else {
+            newBoard[index] = "X"
+            _description.value = "플레이어 1의 차례입니다."
+            if (checkEnd(index, "X")) {
+                _description.value = "게임이 종료되었습니다."
+                end = true
+            }
+        }
+        _board.value = newBoard
+        _isFirstPlayer = !_isFirstPlayer
+    }
+
+    fun checkEnd(currentIndex: Int, currentString: String): Boolean {
+        val rowMod = currentIndex / 3
+        val colMod = currentIndex % 3
+
+        if (_board.value?.get((rowMod * 3) + ((colMod + 1) % 3)) == currentString
+            && _board.value?.get((rowMod * 3) + ((colMod + 2) % 3)) == currentString) {
+            return true
+        }
+
+        if (_board.value?.get(((rowMod + 1) % 3) * 3 + colMod) == currentString
+            && _board.value?.get(((rowMod + 2) % 3) * 3 + colMod) == currentString) {
+            return true
+        }
+
+        if (currentIndex % 2 == 0
+            && _board.value?.get(((rowMod + 1) % 3) * 3 + ((colMod + 1) % 3)) == currentString
+            && _board.value?.get(((rowMod + 2) % 3) * 3 + ((colMod + 2) % 3)) == currentString) {
+            return true
+        }
+
+        return false
+    }
+}
